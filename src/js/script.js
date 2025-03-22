03f2894980e65009b87359d626592e253b028167
yokosaka <rvo9eij8@icloud.com>
2025-03-22 18:23:22 +0900

    about レイアウト見直し　リンク修正中　スムーススクロール実装できない　

--- src/js/script.js
+++ src/js/script.js
@@ -23,36 +23,36 @@ const swiper = new Swiper("#js-gallery-swiper", {
 
 
 
-  // 既存の jQuery のコードも `DOMContentLoaded` 内で実行
-  jQuery("#js-drawer-icon").on("click", function (e) {
-    e.preventDefault();
-    jQuery("#js-drawer-icon").toggleClass("is-checked");
-    jQuery("#js-drawer-content").toggleClass("is-checked");
+// 既存の jQuery のコードも `DOMContentLoaded` 内で実行
+jQuery("#js-drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+});
+
+jQuery(document).ready(function () {
+  // ✅ 最初の .is-open の .question__item-a を表示
+  jQuery(".question__item.is-open .question__item-a").css("display", "flex");
+
+  jQuery(".js-accordion").on("click", function (e) {
+      e.preventDefault();
+
+      const parent = jQuery(this).parent(); // ✅ アコーディオンの親要素
+      const answer = jQuery(this).next(".question__item-a"); // ✅ アコーディオンの答え部分
+      const img = jQuery(this).find(".question__item-button img"); // ✅ 画像ボタン
+
+      if (parent.hasClass("is-open")) {
+          parent.removeClass("is-open");
+          answer.slideUp(300, function () {
+              jQuery(this).removeClass("is-open"); // ✅ クラスを削除
+          });
+          img.attr("src", "./assets/img/q_button+.png"); // ✅ 閉じたときの画像
+      } else {
+          parent.addClass("is-open");
+          answer.addClass("is-open").hide().slideDown(300); // ✅ `is-open` を追加して `display: flex;` を適用
+          img.attr("src", "./assets/img/q_button-.png"); // ✅ 開いたときの画像
+      }
   });
-
-  jQuery(document).ready(function () {
-    // ✅ 最初の .is-open の .question__item-a を表示
-    jQuery(".question__item.is-open .question__item-a").css("display", "flex");
-
-    jQuery(".js-accordion").on("click", function (e) {
-        e.preventDefault();
-
-        const parent = jQuery(this).parent(); // ✅ アコーディオンの親要素
-        const answer = jQuery(this).next(".question__item-a"); // ✅ アコーディオンの答え部分
-        const img = jQuery(this).find(".question__item-button img"); // ✅ 画像ボタン
-
-        if (parent.hasClass("is-open")) {
-            parent.removeClass("is-open");
-            answer.slideUp(300, function () {
-                jQuery(this).removeClass("is-open"); // ✅ クラスを削除
-            });
-            img.attr("src", "./assets/img/q_button+.png"); // ✅ 閉じたときの画像
-        } else {
-            parent.addClass("is-open");
-            answer.addClass("is-open").hide().slideDown(300); // ✅ `is-open` を追加して `display: flex;` を適用
-            img.attr("src", "./assets/img/q_button-.png"); // ✅ 開いたときの画像
-        }
-    });
 });
 
 
@@ -77,43 +77,43 @@ jQuery(".js-modal-close").on("click", function (e) {
 });
 
 
-  jQuery("#js-drawer-content a[href^='#']").on("click", function (e) {
-    jQuery("#js-drawer-content").removeClass("is-checked");
-  });
+jQuery("#js-drawer-content a[href^='#']").on("click", function (e) {
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
 
-  jQuery("a[href^='#']").on("click", function (e) {
-    const speed = 300;
-    const id = jQuery(this).attr("href");
-    const target = jQuery("#" == id ? "html" : id);
-    const position = jQuery(target).offset().top;
-    jQuery("html, body").animate(
-      {
-        scrollTop: position,
-      },
-      speed,
-      "swing" //swing or linear
-    );
-  });
+jQuery("a[href^='#']").on("click", function (e) {
+  const speed = 300;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" == id ? "html" : id);
+  const position = jQuery(target).offset().top;
+  jQuery("html, body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing" //swing or linear
+  );
+});
 
-  jQuery(window).on("scroll", function () {
-    if (100 < jQuery(window).scrollTop()) {
-      jQuery("#js-pagetop").addClass("is-show");
+jQuery(window).on("scroll", function () {
+  if (100 < jQuery(window).scrollTop()) {
+    jQuery("#js-pagetop").addClass("is-show");
+  } else {
+    jQuery("#js-pagetop").removeClass("is-show");
+  }
+});
+
+const intersectionObserver = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-in-view");
     } else {
-      jQuery("#js-pagetop").removeClass("is-show");
+      // entry.target.classList.remove("is-in-view");
     }
   });
+});
 
-  const intersectionObserver = new IntersectionObserver(function (entries) {
-    entries.forEach(function (entry) {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("is-in-view");
-      } else {
-        // entry.target.classList.remove("is-in-view");
-      }
-    });
-  });
-
-  const inViewItems = document.querySelectorAll(".js-in-view");
-  inViewItems.forEach(function (inViewItem) {
-    intersectionObserver.observe(inViewItem);
-  });
+const inViewItems = document.querySelectorAll(".js-in-view");
+inViewItems.forEach(function (inViewItem) {
+  intersectionObserver.observe(inViewItem);
+});
